8011a1fc144da106f56e1780804cd841c7794c5a
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-08-12 02:14:03 -0400

    moved cube

--- src/components/CubeRender.jsx
+++ src/components/CubeRender.jsx
@@ -5,11 +5,11 @@ function CubeRender() {
     useEffect(() => {
         const scrambleText = document.querySelector('#scramble')
         const cubeRender = document.querySelector('#cubeRender');
-        cubeRender.src = `https://visualcube.api.cubing.net/visualcube.php?fmt=svg&size=500&pzl=3&alg=x2${scrambleText.innerHTML}`
+        cubeRender.src = `https://visualcube.api.cubing.net/visualcube.php?fmt=svg&size=250&pzl=3&alg=x2${scrambleText.innerHTML}`
       });
 
     return (
-        <div className="text-center">
+        <div className="position-absolute bottom-0 end-0">
             <img class="img-fluid" id="cubeRender" alt="cube" />
         </div>
     );
